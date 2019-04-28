94bd309add89505b7a53aa6573fd5d9ebd8554a2
Keshav Santhanam <keshav2@stanford.edu>
2019-04-28 15:57:15 -0700

    Send FIN packet after sniffing

--- measure_icw.py
+++ measure_icw.py
@@ -2,17 +2,19 @@ from __future__ import print_function
 
 from scapy.all import *
 import argparse
+import math
 import time
 
 
 def send_syn(dst_ip, src_port, recv_window, mss, verbose=False):
     """Sends a SYN packet and returns the SYN-ACK response.
-   
+
        Args:
           dst_ip: The destination IP address.
           src_port: The source port number.
           recv_window: The receive window size to advertise in bytes.
           mss: The MSS to advertise in bytes.
+          verbose: If True, print debug information.
 
         Returns:
           The SYN-ACK response packet.
@@ -26,38 +28,36 @@ def send_syn(dst_ip, src_port, recv_window, mss, verbose=False):
     SYNACK=sr1(SYN)
     if verbose:
         print('Received SYN-ACK:\n%s' % (SYNACK.show()))
-    
+
     return SYNACK
 
 
 def send_get_request(dst_ip, src_port, seq, ack, verbose=False):
-    """Sends a GET request with a piggybacked ACK. 
+    """Sends a GET request with a piggybacked ACK.
 
        Args:
           dst_ip; The destination IP address.
           src_port: The source port number.
           seq: The sequence number.
           ack: The ack number.
+          verbose: If True, print debug information.
 
         Returns:
           A tuple of a list of the answered packets and a list of unanswered
           packets.
     """
     ip = IP(dst=dst_ip)
-    #get='GET / HTTP/1.0\n\n'
-    get = 'GET /index.html HTTP/1.1\r\n'
+    get = 'GET / HTTP/1.1\r\n'
     GET = ip/TCP(sport=src_port, dport=80, flags="A", seq=seq, ack=ack) / get
     if verbose:
         print('Sending GET request to %s:%d.' % (dst_ip, 80))
-    
+
     ans, unans = sr(GET)
-    
+
     if verbose:
         print('Received GET response:')
         if ans is not None:
-          print('Answered packets:') 
-          import pdb
-          pdb.set_trace()
+          print('Answered packets:')
           print(ans.show())
         else:
             print('No answered packets.')
@@ -66,16 +66,17 @@ def send_get_request(dst_ip, src_port, seq, ack, verbose=False):
             print(unans.show())
         else:
             print('No unanswered packets.')
-                
+
     return ans, unans
 
 
 def sniff_packets(src_port, timeout, verbose=False):
     """Sniffs for packets sent to SRC_PORT.
-   
+
        Args:
           src_port: The source port number.
           timeout: The number of seconds to sniff for.
+          verbose: If True, print debug information.
 
        Returns:
           The list of sniffed packets.
@@ -83,14 +84,36 @@ def sniff_packets(src_port, timeout, verbose=False):
     if verbose:
         print('Sniffing packets sent to port %d for %d seconds...' % (src_port,
                                                                       timeout))
-    packets = sniff(filter="dst port 50000", timeout=10) 
+    packets = sniff(filter="dst port 50000", timeout=timeout)
 
     if verbose:
         print('Sniffed packets:')
         for packet in packets:
             print(packet.show())
-    
-    return packets 
+
+    return packets
+
+
+def send_fin(dst_ip, src_port, verbose=False):
+    """Sends a FIN packet to DST_IP.
+
+       Args:
+          dst_ip: The destination IP address.
+          src_port: The source port number.
+          verbose: If True, print debug information.
+
+       Returns:
+          The response to the FIN packet.
+    """
+    ip = IP(dst=dst_ip)
+    FIN = ip/TCP(sport=src_port, dport=80, flags="FA")
+    if verbose:
+        print('Sending FIN packet to %s:%d.')
+    FIN_response = sr1(FIN)
+    if verbose:
+        print('Received FIN response:\n%s' % (FIN_response.show()))
+
+    return FIN_response
 
 
 def main(args):
@@ -102,6 +125,7 @@ def main(args):
     ans, unans = send_get_request(args.dst_ip, args.src_port, SYNACK.ack,
                                   SYNACK.seq+1, args.verbose)
     all_packets = sniff_packets(args.src_port, args.timeout, args.verbose)
+    FIN_response = send_fin(args.dst_ip, args.src_port, args.verbose)
     unique_packets = {}
     for i, packet in enumerate(all_packets):
         ip_datagram = packet.payload
@@ -109,9 +133,18 @@ def main(args):
         data_len = ip_datagram.len - \
             (ip_datagram.ihl + tcp_datagram.dataofs) * 4
         seq = tcp_datagram.seq
-        print('Packet %d: seq=%d, len=%d' % (i, seq, data_len))
-        unique_packets[seq] = data_len
-    print('ICW=%d' % (sum([unique_packets[seq] for seq in unique_packets])))
+        # print('Packet %d: seq=%d, len=%d' % (i, seq, data_len))
+        if seq in unique_packets:
+            unique_packets[seq] = max(data_len, unique_packets[seq])
+        else:
+            unique_packets[seq] = data_len
+    seqs = sorted([seq for seq in unique_packets])
+    for seq in seqs:
+        print('seq=%d, len=%d' % (seq, unique_packets[seq]))
+    icw_bytes = sum([unique_packets[seq] for seq in unique_packets])
+    icw_segments = int(math.ceil(icw_bytes / float(args.mss)))
+    print('ICW = %d bytes (%d segments)' % (icw_bytes, icw_segments))
+
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(
